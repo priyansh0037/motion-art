84d6590371f14bbe0ac0632098efe87b9a8ddb96
priyansh0037 <negativeam8@gmail.com>
2024-05-21 16:35:08 +0530

    done

--- src/Components/ApplySection.jsx
+++ src/Components/ApplySection.jsx
@@ -4,7 +4,7 @@ import { CardImage } from '../../utils';
 
 const ApplySection = () => {
   return (
-    <div className='px-5  pt-10 pb-16 lg:pt-24  lg:w-5/6 mx-auto relative lg:pointer-events-none h-full'>
+    <div className='px-5  pt-16 pb-24 lg:pt-24  lg:w-5/6 mx-auto relative lg:pointer-events-none h-full'>
       <div className='text-center flex flex-col gap-3'>
         <h2 className='hidden lg:block lg:text-6xl font-semibold text-gray-200'>Apply On Any Section Or Enable</h2>
         <h2 className=' hidden lg:block lg:text-6xl font-semibold text-gray-200'>For Whole Page</h2>
